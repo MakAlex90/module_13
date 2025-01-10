7021e428de711e35ec05eed534b80ec63a55eae8
MakAlex90 <meskalin4ik9@gmail.com>
2025-01-10 15:09:55 +0300

    Update module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -6,7 +6,7 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7786861788:AAFFn2DS9AASUyduh4mRFMZNgA25R4s-jn4"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
