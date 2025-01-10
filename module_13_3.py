b37516893d7adbd5af87ad58da382381653b0ec9
MakAlex90 <meskalin4ik9@gmail.com>
2025-01-10 13:08:11 +0300

    Update module_13_3.py

--- module_13_3.py
+++ module_13_3.py
@@ -2,28 +2,26 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ""
+api =
 bot = Bot(token = api)
-dp = Dispatcher(bot, storage= MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-
-
-@dp.message_handler(text = ["Urban", 'ff'])
+@dp.message_handler(text=["Urban", 'ff'])
 async def urban_message(message):
     print("Urban message")
-    await message.answer ("Urban message!")
+
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    print("Start message")
-    await message.answer("Рады вас видить в нашем боте!")
+    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
 
 @dp.message_handler()
 async def all_message(message):
-    print("Мы получили сообщение!")
-    await message.answer(message.text.upper())
-
+    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
